8b4545b36fe8a1d745c0a36e6578876191b700f9
gunwoocho <gunwoocho2010@gmail.com>
2026-06-02 15:37:47 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,15 +2,19 @@ using BusMafia.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 실시간 웹소켓(SignalR) 서비스 관문 추가
+// 1. 실시간 통신 기능(SignalR) 등록
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-app.UseStaticFiles(); // wwwroot 폴더 내부의 웹 화면을 전송하기 위한 필수 미들웨어
+// 🚨 [매우 중요] 클라우드(리눅스) 환경에서 index.html을 강제로 첫 화면으로 고정하는 코드
+app.UseDefaultFiles(); 
+app.UseStaticFiles();  // wwwroot 폴더를 읽어오기 위한 필수 코드
+
 app.UseRouting();
 
-// 스마트폰 클라이언트가 붙을 무선 기지국 주소 매핑
+// 2. 스마트폰 클라이언트가 접속할 관문 주소 매핑
 app.MapHub<GameHub>("/gameHub");
 
+// 3. 서버 실행
 app.Run();
